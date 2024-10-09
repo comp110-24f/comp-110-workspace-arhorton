12e73f832e48cac1fc53d028176185e38acb1c84
arhorton <arhorton@unc.edu>
2024-10-08 20:52:39 -0400

    post ex04 backup

---
+++ CQs/cq06_sum.py
@@ -0,0 +1,8 @@
+"""Summing the elements of a list using different loops"""
+
+__author__ = "730664552"
+
+def w_sum(vals: list[int]) -> int: 
+    index: int = 0
+    while index < len(vals): 
+        
--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -6,6 +6,8 @@ __author__ = "730664552"
 def all(list: list[int], number: int) -> bool:  # return type is a boolen
     """Tests if all of the ints in a list are the same as the number input"""
     index: int = 0  # starting the index at 0 so that it can scale
+    if len(list) == 0:
+        return False
     while index < len(list):
         if number != list[index]:  # if they don't equal each other
             return False  # return value - helps the function exit early
@@ -16,29 +18,28 @@ def all(list: list[int], number: int) -> bool:  # return type is a boolen
 
 def max(list: list[int]) -> int:
     """Finds the max number in a list"""
-    if len(list) == 0:
-        raise ValueError(
-            "max() arg is an empty list"
-        )  # if an empty list is returned, raise a value error
+    if len(list) == 0:  # empty list error
+        raise ValueError("max() arg is an empty list")
     index: int = 0  # starting the index at zero
-    max: int = 0  # starting the max value at zero so that it can be added to
+    max: int = list[index]  # starting the max value at one so that it can be added to
     while index < len(list):
-        if list[index] > (
-            list[index - 1]
-        ):  # if the value at index is less than 1 before it
+        if list[index] > max:  # if the value at index is less than the original
             max = list[index]  # concetanate the new value to the max list
-        index += 1
-    return max  # return the highest value
+        else:
+            index += 1  # scale the index
+    return max
 
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:  # returns a boolean
     """Tests if all of the values in two lists are exactly equal"""
     index: int = 0  # starting the index at zero
-    while index < len(list1):
+    if len(list1) != len(list2):
+        return False
+    while index < len(list2):
         if list1[index] != list2[index]:
             return False  # lets the function exit early
-        index += 1  # scales the index by one to evaluate the while loop again
-    index += 1
+        index += 1
+    index += 1  # scales the index by one to evaluate the while loop again
     return True  # if the while loop is fine, exit and return True
 
 
